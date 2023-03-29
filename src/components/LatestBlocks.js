1b25631f3a3e36410ae53ea8c009d7fe648c449e
AbhishekPTest <apoojary@deqode.com>
2023-03-29 14:26:29 +0530

    [2023-MARCH] test

--- src/components/LatestBlocks.js
+++ src/components/LatestBlocks.js
@@ -1,4 +1,4 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ColumnArray, ColumnArray_Blocks_List } from "../constants/objectConstants";
 import useWeb3Js from "../customHook/useWeb3Js";
@@ -6,31 +6,31 @@ import styles from "./LatestBlocks.module.css"
 
 
 const LatestBlocks = () => {
-    const {getLatestBlocks} = useWeb3Js([]);
-    const [blockList,setBlockList] =useState([]);
+    const { getLatestBlocks } = useWeb3Js([]);
+    const [blockList, setBlockList] = useState([]);
     const [loading, setLoading] = useState(true);
     const navigate = useNavigate();
 
-    const NavigateToBlockPage=(blockNumber)=>{
-        navigate('/block/'+blockNumber);
+    const NavigateToBlockPage = (blockNumber) => {
+        navigate('/block/' + blockNumber);
     }
-    useEffect(()=>{
-        const fetchList=async()=>{
-            const list= await getLatestBlocks();
+    useEffect(() => {
+        const fetchList = async () => {
+            const list = await getLatestBlocks();
             setBlockList(list);
             setLoading(false);
         }
         fetchList();
-    },[getLatestBlocks])
+    }, [getLatestBlocks])
 
-    if(loading){
+    if (loading) {
         return (
             <h3>Loading</h3>
         );
     }
     return (
         <>
-        <h2>Blocks</h2>
+            <h2>Blocks</h2>
             <div className={styles['custom-container']}>
                 <table>
                     <thead>
@@ -45,22 +45,22 @@ const LatestBlocks = () => {
                     </thead>
 
                     <tbody>
-                            {blockList.length>0 && (
-                                blockList.map((item)=>{
-                                    return (
-                                        <tr key={item.miner}>
-                                            
-                                             <th onClick={()=>NavigateToBlockPage(item.number)}>{item.number}</th>
-                                             <th>{item.timestamp}</th>
-                                             <th>{item.txn}</th>
-                                             <th>{item.miner}</th>
-                                             <th>{item.gasUsed}</th>
-                                            <th>{item.gasLimit}</th>
-                                            <th>{item.baseFeePerGas}</th>
-                                        </tr>
-                                    );
-                                })
-                            )}
+                        {blockList.length > 0 && (
+                            blockList.map((item) => {
+                                return (
+                                    <tr key={item.miner}>
+
+                                        <th onClick={() => NavigateToBlockPage(item.number)}>{item.number}</th>
+                                        <th>{item.timestamp}</th>
+                                        <th>{item.txn}</th>
+                                        <th>{item.miner}</th>
+                                        <th>{item.gasUsed}</th>
+                                        <th>{item.gasLimit}</th>
+                                        <th>{item.baseFeePerGas}</th>
+                                    </tr>
+                                );
+                            })
+                        )}
                     </tbody>
                 </table>
             </div>
